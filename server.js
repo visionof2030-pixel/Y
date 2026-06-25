f0e189ac998767f864ce3e8184510eae85ac4a8e
visionof2030-pixel <visionof2030@gmail.com>
2026-06-25 20:05:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require("express");
-const axios = require("axios");
+const { spawn } = require("child_process");
 
 const app = express();
 
 const STREAM_URL = "http://gazalone.life:8080/live/omar777/01103978590/460864.ts";
 
-app.get("/stream", async (req, res) => {
-  try {
-    const response = await axios({
-      method: "get",
-      url: STREAM_URL,
-      responseType: "stream",
-      headers: {
-        "User-Agent": "Mozilla/5.0"
-      }
-    });
+app.get("/stream.m3u8", (req, res) => {
+  const ffmpeg = spawn("ffmpeg", [
+    "-i", STREAM_URL,
+    "-c:v", "copy",
+    "-c:a", "copy",
+    "-f", "hls",
+    "-hls_time", "3",
+    "-hls_list_size", "3",
+    "-hls_flags", "delete_segments",
+    "pipe:1"
+  ]);
 
-    res.setHeader("Content-Type", "video/mp2t");
+  res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
 
-    response.data.pipe(res);
+  ffmpeg.stdout.pipe(res);
 
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send("stream error");
-  }
+  ffmpeg.stderr.on("data", (data) => {
+    console.log("FFmpeg:", data.toString());
+  });
 });
 
 app.get("/", (req, res) => {
-  res.send("IPTV running");
+  res.send("IPTV FFmpeg Server Running 🚀");
 });
 
 app.listen(process.env.PORT || 3000, () => {
